ff4d714f8194fa2533b2886429ef7939a6ff9b25
KE1ok <bgtstecr33@gmail.com>
2024-08-16 19:54:38 +0900

    アクティブハッシュ、アクティブストレージの導入、バリデーション設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,14 @@
 class ItemsController < ApplicationController
   def index
+    @items = item.order("created_at DESC")
   end
+
+  def new
+    @item = item.new
+  end
+
+  private
+  
+  def item_params
+    params.require(:item).permit(:item_name, :image, :title, :price, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
 end
--- db/migrate/20240816053602_create_items.rb
+++ db/migrate/20240816053602_create_items.rb
@@ -1,10 +1,10 @@
 class CreateItems < ActiveRecord::Migration[7.0]
   def change
     create_table :items do |t|
-      t.string :item_name
-      t.text :title
+      t.string :item_name,         null: false
+      t.text :title,               null: false
       t.integer :price,            null: false
-      t.references :user,          null: false
+      t.references :user, null: false, foreign_key: true
       t.integer :category_id,      null: false
       t.integer :condition_id,     null: false
       t.integer :shipping_fee_id,  null: false
