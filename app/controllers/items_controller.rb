23395f4f66047d369d8ca575019b4f3166c17d7d
KE1ok <bgtstecr33@gmail.com>
2024-08-18 22:37:39 +0900

    商品情報の登録機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,24 @@
 class ItemsController < ApplicationController
   def index
-    @items = item.order("created_at DESC")
+    @items = Item.order("created_at DESC")
   end
 
   def new
-    @item = item.new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
   
   def item_params
     params.require(:item).permit(:item_name, :image, :title, :price, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_date_id).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -6,14 +6,30 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_date
   has_one_attached :image
+  belongs_to :user
 
-  validates :item_name, :string,  presence: true
-  validates :title,     :text,    presence: true
-  validates :price,     :integer, presence: true
+  validates :item_name, presence: true
+  validates :title,     presence: true
+  validates :price,     presence: true, numericality: { 
+    greater_than_or_equal_to: 300, 
+    less_than_or_equal_to: 9999999, 
+    message: "must be between 300 and 9,999,999" 
+  }
+
+  validate :image_attached
 
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_fee_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_date_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+  private
+
+  def image_attached
+    if image.blank?
+      errors.add(:image, "を添付してください")
+    end
+  end
+
 end
