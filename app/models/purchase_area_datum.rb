d3cec2b958589aca52af210e6f22fc2e86ab9e89
KE1ok <bgtstecr33@gmail.com>
2024-09-10 07:01:32 +0900

    保存できる場合のテストコード実装

--- app/models/purchase_area_datum.rb
+++ app/models/purchase_area_datum.rb
@@ -13,6 +13,6 @@ class PurchaseAreaDatum
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    AreaDatum.create(postal_code: postal_code, prefecture_id: prefecture_id, municipalities: municipalities, street: street, building_name: building_name, telephone: telephone, purchase_id: purchase.id)
+    AreaDatum.create(postal_code: postal_code, prefecture: prefecture_id, municipalities: municipalities, street: street, building_name: building_name, telephone: telephone, purchase_id: purchase.id)
   end
 end
--- spec/factories/purchase_area_data.rb
+++ spec/factories/purchase_area_data.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :purchase_area_datum do
     postal_code { '123-4567' }
-    prefecture { 1 }
+    prefecture { 2 }
     municipalities { '東京都' }
     street { '1-1' }
     building_name { '東京ハイツ' }
