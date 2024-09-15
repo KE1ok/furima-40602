e3716fa58ab04def6982e91ad2bd199ee5162ee1
KE1ok <bgtstecr33@gmail.com>
2024-09-15 16:29:39 +0900

    テストコードに、user_idとitem_idを追加

--- spec/models/purchase_area_datum_spec.rb
+++ spec/models/purchase_area_datum_spec.rb
@@ -74,6 +74,16 @@ RSpec.describe PurchaseAreaDatum, type: :model do
         @purchase_area_datum.valid?
         expect(@purchase_area_datum.errors.full_messages).to include("Token can't be blank")
       end
+      it 'user_idが空では登録できないこと' do
+        @purchase_area_datum.user_id = nil
+        @purchase_area_datum.valid?
+        expect(@purchase_area_datum.errors.full_messages).to include("User can't be blank")
+      end
+      it 'item_idが空では登録できないこと' do
+        @purchase_area_datum.item_id = nil
+        @purchase_area_datum.valid?
+        expect(@purchase_area_datum.errors.full_messages).to include("Item can't be blank")
+      end
     end
   end
 end
